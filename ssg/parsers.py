57483bbd8a637e7e4c2b593281c1567bd5202b0b
Olivia0414 <zhangshuyue0414@gmail.com>
2022-01-28 19:39:09 +0100

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -9,21 +9,16 @@ class Parser:
     def valid_extension(self, extension):
         return extension in self.extensions
 
-    def parse(self, path, source, dest):
-        try:
-            self.path = Path(path)
-            self.source = Path(source)
-            self.dest = Path(dest)
-        except NotImplementedError as e:
-            raise
+    def parse(self, path : Path, source : Path, dest : Path):
+        raise NotImplementedError
 
     def read(self, path):
-        with open(path, mode='rt', encoding='utf-8') as file:
+        with open(path, "r") as file:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
         full_path = self.dest / path.with_suffix(ext).name
-        with open(full_path, mode='wt', encoding='utf-8') as file:
+        with open(full_path,"w") as file:
             file.write(content)
 
     def copy(self, path, source, dest):
