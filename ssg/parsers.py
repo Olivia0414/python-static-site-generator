b805e75249dfd0c71671a1a9ae5ab0fac4d1f320
Olivia0414 <zhangshuyue0414@gmail.com>
2022-01-28 20:59:23 +0100

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,6 +1,10 @@
 from typing import List
 from pathlib import Path
 import shutil
+import sys
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
 
 class Parser:
     """docstring for Parser."""
@@ -30,3 +34,27 @@ class ResourceParser(Parser):
 
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
+
+class MarkdownParser(Parser):
+    """docstring for ."""
+    extensions = [".md", ".markdown"]
+
+#    def __init__(self, arg):
+#        super(, self).__init__()
+#        self.arg = arg
+
+    def parse(self, path : Path, source : Path, dest : Path):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html, ext=".html")
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+
+class ReStructuredTextParser(Parser):
+    """docstring for ."""
+    extensions = [".rst"]
+
+    def parse(self, path : Path, source : Path, dest : Path):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"], ext=".html")
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
