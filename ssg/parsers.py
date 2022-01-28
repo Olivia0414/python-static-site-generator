cbaebbf2abd13df724e76818e84c76c76498fe6b
Olivia0414 <zhangshuyue0414@gmail.com>
2022-01-28 21:04:17 +0100

    Fixed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -47,7 +47,7 @@ class MarkdownParser(Parser):
         content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path, dest, html, ext=".html")
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 class ReStructuredTextParser(Parser):
     """docstring for ."""
@@ -56,5 +56,5 @@ class ReStructuredTextParser(Parser):
     def parse(self, path : Path, source : Path, dest : Path):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        self.write(path, dest, html["html_body"], ext=".html")
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
